59feed739bb003d7a95c88062d5be9ff221dc0fe
ofirzvishaboo <ofirfirst@gmail.com>
2023-08-21 17:36:53 +0300

    Finished

--- RegisterPage.py
+++ RegisterPage.py
@@ -16,35 +16,43 @@ class RegisterPage(BasePage):
 
     def __init__(self, driver):
         super().__init__(driver)
+        # Setting my name for assertion later
         self.name = "ofir"
 
     def click_signup_button(self):
+        # Click on signuo/login
         self.click_element(*self.SELECTORS["NOT_SIGNED"])
 
     def click_signup_text(self):
+        # Click on signup text
         self.click_element(*self.SELECTORS["SIGNUP_TEXT"])
 
     def enter_name(self):
+        # Enters first name
         self.enter_text(*self.SELECTORS["NAME_INPUT"], self.name)
 
     def enter_mail(self):
+        # Enters Email address
         self.enter_text(*self.SELECTORS["EMAIL_INPUT"], "forifnnri@gmail.com")
 
     def enter_password(self):
+        # Enters Password
         self.enter_text(*self.SELECTORS["PASSWORD_INPUT"], "Ohoh1123654")
 
     def enter_password_confirmation(self):
+        # Entering password confirmation
         self.enter_text(*self.SELECTORS["CONFIRM_PASSWORD_INPUT"], "Ohoh1123654")
 
     def click_login_checkbox(self):
+        # Click on stay logged in checkbox
         self.click_element(*self.SELECTORS["LOGIN_CHECKBOX"])
 
     def click_submit(self):
+        # Moving to the submit button
         self.move_to_element(*self.SELECTORS["SUBMIT_BUTTON"])
+        # Click on Submit button
         self.click_element(*self.SELECTORS["SUBMIT_BUTTON"])
 
-    def get_screenshot(self):
-        self.screenshot("SUBMIT_BUTTON")
-
     def firstname_assertion(self):
+        # Asserting the first name was correct
         self.assert_input(*self.SELECTORS["NAME_INPUT"], self.name)
--- SendGifts.py
+++ SendGifts.py
@@ -20,31 +20,46 @@ class SendGifts(BasePage):
         self.name = "ofir"
 
     def receiver_name(self):
+        # Enters receiver name
         self.enter_text(*self.SELECTORS["RECEIVER_NAME_INPUT"], "ohad")
 
     def occasion(self):
+        # Clicks on selectbox
         self.click_element(*self.SELECTORS["OCCASION_SELECT"])
+        # Choosing birthday
         self.force_click(*self.SELECTORS["OCCASION_SELECT_CLICK"])
 
     def birthday_card(self, card):
+        # Moving to the birthday card
         self.move_to_element(*self.SELECTORS["BIRTHDAY_CARD_TEXTAREA"])
+        # Enter the birthday card that i will write on the main
         self.enter_text(*self.SELECTORS["BIRTHDAY_CARD_TEXTAREA"], card)
 
     def add_picture(self):
         filepath = "/Users/ofirzvishaboo/Downloads/my_logo.png"
+        # Moving to the photo upload
         self.move_to_element(*self.SELECTORS["PHOTO_UPLOAD"])
+        # Uploading the photo
         self.send_photo(*self.SELECTORS["PHOTO_UPLOAD"], filepath)
 
     def click_continue(self):
+        # Moving to continue button
         self.move_to_element(*self.SELECTORS["CONTINUE_BUTTON"])
+        # Click on continue button
         self.click_element(*self.SELECTORS["CONTINUE_BUTTON"])
 
     def choose_sms(self):
+        # Click on SMS
         self.click_element(*self.SELECTORS["CHOOSE_SMS_SVG"])
+        # Clicks on textbox
         self.click_element(*self.SELECTORS["SMS_INPUT"])
+        # Entering the text
         self.enter_text(*self.SELECTORS["SMS_INPUT"], "0525369535")
 
     def gift_sender(self):
+        # Moving to gift sender name
         self.move_to_element(*self.SELECTORS["GIFT_SENDER_INPUT"])
+        # Enters my name form init
         self.enter_text(*self.SELECTORS["GIFT_SENDER_INPUT"], self.name)
+        # Asserting that i got the right name
         self.assert_input(*self.SELECTORS["GIFT_SENDER_INPUT"], self.name)
